ae8ed4ce830995c5ebcba578e73fd8eb25db5269
xiao-mac <543033627@qq.com>
2022-11-11 17:54:52 +0800

    三种消息普通消息 1.普通消息 2.带tag消息 3.带tag和key的消息

--- learn_rocketmq/mq-consumer/src/main/java/com/xiao/mqconsumer/consumer/GeneralMessagesConsumer.java
+++ learn_rocketmq/mq-consumer/src/main/java/com/xiao/mqconsumer/consumer/GeneralMessagesConsumer.java
@@ -1,23 +1,60 @@
 package com.xiao.mqconsumer.consumer;
 
-import com.xiao.mqproducer.entity.TestMessage;
+
+import com.xiao.mqcommon.entity.TestMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author aloneMan
  * @projectName plan-message-middleware
  * @createTime 2022-11-11 15:47:36
- * @description
+ * @description 消费者
  */
 @Slf4j
 @Component
-@RocketMQMessageListener(topic = "general_two_topic", consumerGroup = "general_two")
-public class GeneralMessagesConsumer implements RocketMQListener<TestMessage> {
-    @Override
-    public void onMessage(TestMessage message) {
-        log.info("{} ", message);
+public class GeneralMessagesConsumer {
+
+    /**
+     * 消费主题为general_topic的所有消息
+     */
+    @Service
+    @RocketMQMessageListener(topic = "general_topic", consumerGroup = "consumer_one_group")
+    public class ConsumerOne implements RocketMQListener<MessageExt> {
+
+        @Override
+        public void onMessage(MessageExt message) {
+            log.info("消费者是 >>> {} 收到的消息是 >>> {} tag >>> {}", "One", new String(message.getBody()), message.getTags());
+        }
+    }
+
+    /**
+     * 接受主题为general_topic，并且tag的主题是tag_n1
+     */
+    @Service
+    @RocketMQMessageListener(topic = "general_topic", selectorExpression = "tag_n1", consumerGroup = "consumer_two_group")
+    public class ConsumerTwo implements RocketMQListener<MessageExt> {
+        @Override
+        public void onMessage(MessageExt message) {
+            log.info("消费者是 >>> {} 收到的消息是 >>> {} tag >>> {}", "Two", new String(message.getBody()), message.getTags());
+        }
+    }
+
+    /**
+     * 接受主题为general_topic，并且tag的主题是tag_n2的消息，此消息携带了key
+     */
+    @Service
+    @RocketMQMessageListener(topic = "general_topic", selectorExpression = "tag_n2", consumerGroup = "consumer_three_group")
+    public class ConsumerThree implements RocketMQListener<MessageExt> {
+        @Override
+        public void onMessage(MessageExt message) {
+            log.info("消费者是 >>> {} 收到的消息是 >>> {} tag >>> {}", "Three", new String(message.getBody()), message.getTags());
+        }
     }
 }
--- learn_rocketmq/mq-producer/src/main/java/com/xiao/mqproducer/producer/GeneralMessages.java
+++ learn_rocketmq/mq-producer/src/main/java/com/xiao/mqproducer/producer/GeneralMessages.java
@@ -1,9 +1,13 @@
 package com.xiao.mqproducer.producer;
 
-import com.xiao.mqproducer.entity.TestMessage;
+
+import com.xiao.mqcommon.entity.TestMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -28,34 +33,53 @@ public class GeneralMessages {
     private AtomicLong count = new AtomicLong();
     private final RocketMQTemplate rocketMQTemplate;
 
+
     public GeneralMessages(RocketMQTemplate rocketMQTemplate) {
         this.rocketMQTemplate = rocketMQTemplate;
     }
 
-    @PostMapping("/sendOne")
-    public Object send() {
+    /**
+     * 发送普通消息
+     *
+     * @return
+     */
+    @PostMapping("sendGeneralMessage")
+    public void sendGeneralMessage() {
+        String body = "Send general message";
         TestMessage testMessage = new TestMessage();
-        long andIncrement = count.getAndIncrement();
-        testMessage.setMessageId(andIncrement);
-        testMessage.setMessageBody("producer " + andIncrement);
-        HashMap<String, Object> headers = new HashMap<>(1);
-        headers.put("KEYS", "hello");
-        MessageHeaderAccessor messageHeaderAccessor = new MessageHeaderAccessor();
-        messageHeaderAccessor.setHeader("keys", "hello");
-        SendResult sendResult = rocketMQTemplate.syncSend("general_topic:general_tag", MessageBuilder.withPayload(testMessage).setHeaders(messageHeaderAccessor).build());
-        log.info("{} ", sendResult);
-        return testMessage;
+        testMessage.setMessageId(count.getAndIncrement());
+        testMessage.setMessageBody(body + count.get());
+        Message<TestMessage> message = MessageBuilder.withPayload(testMessage).build();
+        rocketMQTemplate.convertAndSend("general_topic", message);
     }
 
-    @PostMapping("/sentTwo")
-    public Object sendTwo() {
-        long andIncrement = count.getAndIncrement();
+
+    /**
+     * 发送带tag的消息
+     *
+     * @return
+     */
+    @PostMapping("sendMessageCarryTag")
+    public void sendMessageCarryTag() {
+        String body = "Send carry tag  message";
         TestMessage testMessage = new TestMessage();
-        testMessage.setMessageId(andIncrement);
-        testMessage.setMessageBody("sendTwo >> " + andIncrement);
-        Map<String, Object> stringObjectMap = new HashMap<>(1);
-        stringObjectMap.put("KEYS", andIncrement);
-        rocketMQTemplate.convertAndSend("general_two_topic:t1", testMessage, stringObjectMap);
-        return testMessage;
+        testMessage.setMessageId(count.getAndIncrement());
+        testMessage.setMessageBody(body + count.get());
+        Message<TestMessage> message = MessageBuilder.withPayload(testMessage).build();
+        rocketMQTemplate.convertAndSend("general_topic:tag_n1", message);
+    }
+
+    /**
+     * 发送带tag 和 key的消息
+     */
+    @PostMapping("/sendMessageCarryTagAndKey")
+    public void send() {
+        String body = "Send carry tag and key  message";
+        TestMessage testMessage = new TestMessage();
+        testMessage.setMessageBody(body + count);
+        long count = this.count.getAndIncrement();
+        testMessage.setMessageId(count);
+        Message<TestMessage> message = MessageBuilder.withPayload(testMessage).setHeader(RocketMQHeaders.KEYS, UUID.randomUUID().toString()).setHeader("age", count).build();
+        rocketMQTemplate.convertAndSend("general_topic:tag_n2", message);
     }
 }
